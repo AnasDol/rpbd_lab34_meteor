311158419716776ff02e543c06294c58aeb4e9ca
AnasDol <anastasiadol2002@gmail.com>
2023-11-26 15:00:32 +0700

    TaskList switched to CollectionList

--- rpbd34/client/main.jsx
+++ rpbd34/client/main.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
-import TaskList from '../imports/ui/TaskList';
+import CollectionList from '../imports/ui/CollectionList';
 import AddTaskForm from '../imports/ui/AddTaskForm';
 
 Meteor.startup(() => {
   render(
     <>
-      <TaskList />
+      <CollectionList collectionName="tasks" />
       <AddTaskForm />
     </>,
     document.getElementById('app')
