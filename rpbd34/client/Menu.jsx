dcda2af88d1d0a5b2b0bc01faea9d6ccfcf9626b
AnasDol <anastasiadol2002@gmail.com>
2023-12-12 13:46:13 +0700

    css update

--- rpbd34/client/Menu.jsx
+++ rpbd34/client/Menu.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../imports/ui/styles.css';
 
+import { useNavigate } from 'react-router-dom';
+import { useTracker } from 'meteor/react-meteor-data';
+
 const Menu = ({ setActiveTab }) => {
+  const navigate = useNavigate();
+
   const handleBreedListClick = () => {
     setActiveTab('breedList');
   };
@@ -14,17 +20,48 @@ const Menu = ({ setActiveTab }) => {
     setActiveTab('taskList');
   };
 
+  const handleLogout = () => {
+    Meteor.logout((error) => {
+      if (error) {
+        console.error('Logout error:', error.reason);
+      } else {
+        localStorage.removeItem('authToken');
+        navigate('/login');
+      }
+    });
+  };
+
+  const user = useTracker(() => Meteor.user());
+  const isAuthenticated = !!localStorage.getItem('authToken');
+
   return (
     <div className="menu-container">
-      <button className="menu-button" onClick={handleBreedListClick}>
-        Породы
-      </button>
-      <button className="menu-button" onClick={handleClientListClick}>
-        Клиенты
-      </button>
-      <button className="menu-button" onClick={handleTaskListClick}>
-        Task List
-      </button>
+      <div className="left-buttons">
+        <button className="menu-button" onClick={handleBreedListClick}>
+          Породы
+        </button>
+        <button className="menu-button" onClick={handleClientListClick}>
+          Клиенты
+        </button>
+        <button className="menu-button" onClick={handleTaskListClick}>
+          Task List
+        </button>
+      </div>
+      <div className="right-buttons">
+        {isAuthenticated && (
+          <div className="user-info">
+            <p className="username">{user ? user.username : 'Unknown'}</p>
+            <button className="logout-button" onClick={handleLogout}>
+              Выйти
+            </button>
+          </div>
+        )}
+        {!isAuthenticated && (
+          <Link to="/login" className="menu-button">
+            Войти
+          </Link>
+        )}
+      </div>
     </div>
   );
 };
